8dbd3d87000051950687db70d984051b3f313ab6
Lim Xue Xian <Lim Xue Xian@LAPTOP-FAJVH508>
2021-10-13 20:14:50 +0100

    Exercise 2

--- main.c
+++ main.c
@@ -24,6 +24,9 @@ void main(void) {
     LATDbits.LATD7=0;   //set initial output state
     TRISDbits.TRISD7=0; //set TRIS value for pin (output)
     
+    LATHbits.LATH3=0; //set initial output state
+    TRISHbits.TRISH3=0; //set TRIS value for pin (output)
+    
     // setup pin for input (connected to button)
     TRISFbits.TRISF2=1; //set TRIS value for pin (input)
     ANSELFbits.ANSELF2=0; //turn off analogue input on pin  
@@ -32,8 +35,10 @@ void main(void) {
         
         while (PORTFbits.RF2); //empty while loop (wait for button press)
         
-        if (!PORTFbits.RF2) LATDbits.LATD7 = !LATDbits.LATD7; //toggle LED
+            if (!PORTFbits.RF2) LATDbits.LATD7 = !LATDbits.LATD7; //toggle LED
+        
+            LATHbits.LATH3 = !LATDbits.LATD7; //set the 2 LED states to be opposite
 
-        __delay_ms(200); // call built in delay function 
+            __delay_ms(200); // call built in delay function 
     }
 }
